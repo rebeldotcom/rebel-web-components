d1555d7ab76a6649b5c74e65640c18237c52a841
Michael Kallies <michaelkallies88@gmail.com>
2019-11-22 11:24:45 -0500

    pass props to stack

--- src/components/stack/stack.tsx
+++ src/components/stack/stack.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import Box from '../box'
 
 const Stack = ({ children, ...rest }) => {
-  return <Box flexDirection="column">{children}</Box>
+  return (
+    <Box flexDirection="column" {...rest}>
+      {children}
+    </Box>
+  )
 }
 
 export default Stack
