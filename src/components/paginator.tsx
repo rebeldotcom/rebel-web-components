f508d479c87e35efa49cdba10dabbce2bca26864
curtis.gould <curtis.gould@rebel.com>
2025-02-24 14:33:13 -0500

    fix eslint errors

--- src/components/paginator.tsx
+++ src/components/paginator.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useLayoutEffect } from 'react'
-import styled from 'styled-components'
-import { css } from "styled-components"
+import styled, { css } from 'styled-components'
 import Box, { BoxProps } from './box'
 import Text from './text'
 import Stack from './stack'
@@ -42,85 +41,77 @@ function Paginator({
   showResultCount = false,
   ...props
 }: PaginationProps): React.ReactNode {
-  const [current, setCurrent] = useState(1)
+  const [current, setCurrent] = useState<number>(1)
   const [countPerPage, setCountPerPage] = useState<number>(rowsPerPage)
   let offset = (current - 1) * countPerPage
-  const didMount = useRef(true) // used to track if this is the first time the component renders
-  
-  /* Handles when the user clicks on a page number */
-  const onPageSelect = (page: number, idx: number) => {
+  const didMount = useRef<boolean>(true)
+
+  const onPageSelect = (page: number) => {
     setCurrent(page)
   }
 
   const getPaginationPages = () => {
     const pages = []
-    const expectedVisiblePagesCount = 5 //expect 5 total: start ... 3 inbetween ... end 
+    const expectedVisiblePagesCount = 5 // expect 5 total: start ... 3 inbetween ... end
     const totalPages = Math.ceil(total / countPerPage)
 
-    let forwardPages = 2
-    let backwardPages = 1
-    
-    let start = ((current - backwardPages) <= 1) ? 2 : (current - backwardPages)
-    let end = ((current + forwardPages) >= totalPages) ? (totalPages - 1) : (current + forwardPages)
+    const forwardPages = 2
+    const backwardPages = 1
 
-    //handle edge cases for start and end
-    if(totalPages > expectedVisiblePagesCount){//we have more pages than we want to show at once
+    let start = current - backwardPages <= 1 ? 2 : current - backwardPages
+    let end =
+      current + forwardPages >= totalPages
+        ? totalPages - 1
+        : current + forwardPages
 
+    if (totalPages > expectedVisiblePagesCount) {
       const middlePages = expectedVisiblePagesCount - 3 // -3 because start and end always show and 1 more because indexes are -1
 
-      //check that forward pages will not look silly when we near start index bounds
-      if ((current + forwardPages) >= totalPages){
+      // check that forward pages will not look silly when we near start index bounds
+      if (current + forwardPages >= totalPages) {
         start = end - middlePages
       }
 
-      //check that backward pages will not look silly when we near end index bounds
-      if ((current - backwardPages) <= 1){
-        end = start + middlePages 
+      // check that backward pages will not look silly when we near end index bounds
+      if (current - backwardPages <= 1) {
+        end = start + middlePages
       }
-
-
-    } else { //just show all pages
+    } else {
+      // just show all pages
       start = 2
       end = totalPages
     }
 
-
-
-    //there is always page 1
+    // there is always page 1
     pages.push(1)
 
-    //add elipsis if our index is past page 2 and if there is more pages than our expected max visible
-    if (start > 2 && totalPages > expectedVisiblePagesCount) pages.push("...")
+    // add elipsis if our index is past page 2 and if there is more pages than our expected max visible
+    if (start > 2 && totalPages > expectedVisiblePagesCount) pages.push('...')
 
-    // Add the range of pages
     for (let i = start; i <= end; i++) {
       pages.push(i)
     }
-  
-    // Add "..." if there needs to be a gap before the last page
-    if (end < totalPages - 1 && totalPages > expectedVisiblePagesCount) pages.push("...")
-  
+
+    if (end < totalPages - 1 && totalPages > expectedVisiblePagesCount)
+      pages.push('...')
+
     // Always include the last page
     if (totalPages > 1) pages.push(totalPages)
-  
+
     return pages
   }
 
-  /* Handles when user clicks the next button */
   const prevButton = () => {
     if (current > 1) {
       const prevPage = current - 1
-      const prevIndex = prevPage - 1
-      onPageSelect(prevPage, prevIndex)
+      onPageSelect(prevPage)
     }
   }
 
-  /* Handles when the user clicks the prev button */
   const nextButton = () => {
     if (current < Math.ceil(total / countPerPage)) {
       const nextPage = current + 1
-      const nextIndex = nextPage - 1
-      onPageSelect(nextPage, nextIndex)
+      onPageSelect(nextPage)
     }
   }
 
@@ -151,103 +142,124 @@ function Paginator({
       {...props}
     >
       {/* Primary Pagination logic  */}
-      <Box key="pagination" alignItems="center" fontFamily="Inter" justifyContent="center">
-          <Box
-            style={{ gap: "8px", borderRadius: "1234px", cursor: "pointer", userSelect: "none" }}
-            border-radius="1234px"
-            pt={12} pr={20} pb={12} pl={20}
-            fontWeight="semi"
-            mr={2}       
-            color="neutral-900" 
-            opacity="0px"            
-            onClick={prevButton}
-            bg="neutral-50"
-            border="1px solid #E5E7EB"
-          >
-            <Icon
-              style={{userSelect: "none", marginTop:"1px"}}
-              name="chevron-left"
-              height={24} width={24}
-            />Previous
-          </Box>
-        <Box>{/* might need to do something with length, grows and shrinks which is annoying */}
-        {getPaginationPages().map((page, idx) =>
-          page === "..." ? (
-            <span key={`ellipsis-${idx}`} style={{ margin: "0 8px 0 0" }}>
-              ...
-            </span>
-          ) : (
-            <PaginationButton
-              css={css`padding: 4px 10px 0 10px`} 
-              fontWeight="semi" 
-              fontSize={12}
-              key={`page-${page}-${idx}`}
-              style={{ cursor: "pointer" }}
-              bg={current === page ? "magenta" : "neutral-50"}
-              color={current === page ? "white" : "black"}
-              mr={3}
-              onClick={(e) => {
-                  const pageNumber = parseInt(page.toString())
-                  onPageSelect(pageNumber, pageNumber - 1)
-              }}
-            >
-              <Text>{page}</Text>
-            </PaginationButton>
-          )
-        )}
-        </Box>
-        <Box          
-        style={{ gap: "8px", borderRadius: "1234px", cursor: "pointer", userSelect: "none" }}
-        pt={12} pr={20} pb={12} pl={20}  
-        opacity="0px"
-        bg="neutral-50"
-        color="neutral-900"
-        border="1px solid #E5E7EB"
-        fontWeight="semi"
-        onClick={nextButton}
+      <Box
+        key="pagination"
+        alignItems="center"
+        fontFamily="Inter"
+        justifyContent="center"
+      >
+        <Box
+          bg="neutral-50"
+          border="1px solid"
+          borderColor="neutral-200"
+          borderRadius="rounded"
+          color="neutral-900"
+          fontWeight="semi"
+          mr={2}
+          onClick={prevButton}
+          opacity="0px"
+          px={20}
+          py={12}
+          style={{
+            gap: '8px',
+            cursor: 'pointer',
+            userSelect: 'none',
+          }}
         >
-          Next<Icon height={24} width={24} name="chevron-right" style={{userSelect: "none", marginTop: "1px"}} />
+          <Icon
+            height={24}
+            name="chevron-left"
+            style={{ userSelect: 'none', marginTop: '1px' }}
+            width={24}
+          />
+          Previous
+        </Box>
+        <Box>
+          {getPaginationPages().map((page, idx) =>
+            page === '...' ? (
+              <span key={`ellipsis-${idx}`} style={{ margin: '0 8px 0 0' }}>
+                ...
+              </span>
+            ) : (
+              <PaginationButton
+                key={`page-${page}-${idx}`}
+                bg={current === page ? 'magenta' : 'neutral-50'}
+                color={current === page ? 'white' : 'black'}
+                css={css`
+                  padding: 4px 10px 0 10px;
+                `}
+                fontSize={12}
+                fontWeight="semi"
+                mr={3}
+                onClick={() => {
+                  const pageNumber = Number(page.toString())
+                  onPageSelect(pageNumber)
+                }}
+                style={{ cursor: 'pointer' }}
+              >
+                <Text>{page}</Text>
+              </PaginationButton>
+            )
+          )}
+        </Box>
+        <Box
+          bg="neutral-50"
+          border="1px solid"
+          borderColor="neutral-200"
+          borderRadius="rounded"
+          color="neutral-900"
+          fontWeight="semi"
+          onClick={nextButton}
+          opacity="0px"
+          px={20}
+          py={12}
+          style={{
+            gap: '8px',
+            cursor: 'pointer',
+            userSelect: 'none',
+          }}
+        >
+          Next
+          <Icon
+            height={24}
+            name="chevron-right"
+            style={{ userSelect: 'none', marginTop: '1px' }}
+            width={24}
+          />
         </Box>
       </Box>
-      {/* Display the number of results, and the range of data being shown */}
       {showResultCount && (
-        <Text 
-        color="neutral-400"
-        width={225}
-        ml={2}
-        mr={2} 
-        mt={3}>
-          Showing {offset + 1} to {total < countPerPage ? total : current * countPerPage}{' '}
-          of {total}
+        <Text color="neutral-400" ml={2} mr={2} mt={3} width={225}>
+          Showing {offset + 1} to{' '}
+          {total < countPerPage ? total : current * countPerPage} of {total}
         </Text>
       )}
-      {/* Display a selector to choose number of rows to display */}
       {showRowSelector && (
-        <Box mr={4} mt={1} color="neutral-400">
+        <Box color="neutral-400" mr={4} mt={1}>
           <Text alignSelf="center" mr={2} variant="milliBold">
             Rows per page:
           </Text>
           <Select
             css={css`
               select {
-                border:none;
-                width:auto;
+                border: none;
+                width: auto;
                 padding: 0 0 0 0;
-                margin-top:8px;
-                font-weight:600;
-                font-size:1.2rem;
-                color: #9CA3AF;
+                margin-top: 8px;
+                font-weight: 600;
+                font-size: 1.2rem;
+                color: #9ca3af;
               }
-              div{
-                padding-top:0;
-              }               
+              div {
+                padding-top: 0;
+              }
             `}
-            pt={2}
             id="row-selector"
             onChange={e => {
-              onRowSelectChange(parseInt(e.toString()))
+              onRowSelectChange(Number(e.toString()))
             }}
             options={rowsPerPageOptions}
+            pt={2}
             value={countPerPage}
           />
         </Box>
